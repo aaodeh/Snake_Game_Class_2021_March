6833d7e3b36dcdb8e5b84e20a922f708bec964b7
aaodeh <66892536+aaodeh@users.noreply.github.com>
2021-05-03 19:00:35 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [snake, setSnake] = useState([
+    [0, 0],
+    [2, 0],
+    [4, 0],
+    [6, 0],
+    [8, 0]
+  ]);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      moveSnake();
+    }, 300);
+
+    return () => clearInterval(interval);
+  });
+
+  const moveSnake = () => {
+    let newSnake = snake.map((x) => x);
+    let head = newSnake[newSnake.length - 1];
+
+    head = [head[0], head[1] + 2];
+
+    newSnake.push(head);
+    newSnake.shift();
+
+    setSnake(newSnake);
+  };
+
+  return (
+    <div className="App">
+      <div className="game-area">
+        {snake.map((s) => (
+          <div
+            className="snake-cell"
+            style={{ top: `${s[1]}%`, left: `${s[0]}%` }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
